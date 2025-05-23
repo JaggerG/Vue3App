fd817c7ba88815d8615c984fd4ed8ceb180fb56c
姜港 <890060@leapmotor.com>
2025-05-23 13:41:49 +0800

    feat: 新增日历组件

---
+++ src/views/calendar/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <Calendar />
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Calendar from '@/components/Calendar.vue';
+export default defineComponent({
+    name: 'CalendarPage',
+    components: {
+        Calendar
+    }
+})
+</script>
+<style scoped lang="scss"></style>
